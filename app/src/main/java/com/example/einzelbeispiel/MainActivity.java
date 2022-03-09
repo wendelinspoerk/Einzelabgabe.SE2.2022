c78070b401b40bde9c97812314881b850aff1818
Wendelin Spörk <wendelinspork@MacBook-Pro-von-Wendelin.fritz.box>
2022-03-09 19:58:08 +0100

    TCP Client implementier. Funktioniert nicht nicht, muss noch gestartet werden und die Permission im Manifest hinzugefügt werden

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +31,38 @@ public class MainActivity extends AppCompatActivity {
 
         button1 = findViewById(R.id.button1);
         ausgabe_server = findViewById(R.id.ausgabe_server);
-
         Matrikelnummer = findViewById(R.id.Matrikelnummer);
         button2 = findViewById(R.id.button2);
         ausgabe_Berechnung = findViewById(R.id.ausgabe_berechnung);
 
 
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+
+
+
+    }
+
+    public void starClient(){
+        TCPClient thread = new TCPClient();
+        thread.start();
+    }
+
+    public class TCPClient extends Thread {
+        public void main(String argv[]) throws Exception {
+            String sentence = Matrikelnummer.getText().toString();
+
+            Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outToServer.writeBytes(sentence + '\n');
+
+            ausgabe_server.setText(inFromServer.readLine());
+            clientSocket.close();
+        }
     }
 }
