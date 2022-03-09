96f4e984146f2f98f3f96674841acccd66f82925
Wendelin Spörk <wendelinspork@0b86aa59-4498-4e2d-88dc-640f37bed529.fritz.box>
2022-03-09 19:36:34 +0100

    Buttons, TextViews und EditText deklariert und Variablen zugewiesen

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    TextView ausgabe_server;
+
+
+    EditText Matrikelnummer;
+    Button button2;
+    TextView ausgabe_Berechnung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        ausgabe_server = findViewById(R.id.ausgabe_server);
+
+        Matrikelnummer = findViewById(R.id.Matrikelnummer);
+        button2 = findViewById(R.id.button2);
+        ausgabe_Berechnung = findViewById(R.id.ausgabe_berechnung);
+
+
+
     }
 }
